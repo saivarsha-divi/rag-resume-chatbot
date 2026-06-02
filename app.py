1180ee125270b601e41dbdd45f68f2aa02f993b7
saivarsha-divi <chowdaryvarsha03@gmail.com>
2026-06-02 12:03:21 +0000

    completed RAG demo

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from dotenv import load_dotenv
-
-from langchain_community.document_loaders import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_openai import ChatOpenAI
 
-load_dotenv()
+# Load documents
+documents = []
 
-loader = DirectoryLoader("data")
-documents = loader.load()
+for file in [
+    "data/leave_policy.txt",
+    "data/work_from_home.txt",
+    "data/travel_policy.txt"
+]:
+    loader = TextLoader(file)
+    documents.extend(loader.load())
 
+# Split documents
 splitter = RecursiveCharacterTextSplitter(
     chunk_size=300,
     chunk_overlap=50
@@ -18,37 +22,23 @@ splitter = RecursiveCharacterTextSplitter(
 
 docs = splitter.split_documents(documents)
 
-embeddings = OpenAIEmbeddings()
-
-db = FAISS.from_documents(
-    docs,
-    embeddings
+# Free embeddings
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
+# Create vector database
+db = FAISS.from_documents(docs, embeddings)
+
+# User question
 question = input("Ask a question: ")
 
-retriever = db.as_retriever()
-
+# Retrieve relevant documents
+retriever = db.as_retriever(search_kwargs={"k": 1})
 results = retriever.invoke(question)
 
-context = "\n".join(
-    [doc.page_content for doc in results]
-)
+print("\nRelevant Information:\n")
 
-llm = ChatOpenAI(
-    model="gpt-4o-mini"
-)
-
-response = llm.invoke(
-f"""
-Answer using only the context.
-
-Context:
-{context}
-
-Question:
-{question}
-"""
-)
-
-print(response.content)
+for doc in results:
+    print(doc.page_content)
+    print("-" * 50)
